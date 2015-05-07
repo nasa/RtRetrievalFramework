0ca3af2c14836ea9d011418a058f40eaeeaa7953
James McDuffie <James.McDuffie@jpl.nasa.gov>
2015-05-07 12:00:59 -0700

    Fix UQ utility so that the sampled_sv from the UQ file is only used for the measured radiance calculation, with the original initial guess loaded from Lua reset before the retrieval starts.

--- support/utils/uq_experiment.py
+++ support/utils/uq_experiment.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from contextlib import closing
 
 import h5py
 import numpy
@@ -27,28 +28,28 @@ class UqExperiment(object):
         # Number of spectrometers
         self.num_spec = self.lua_config.number_pixel.rows(self.lua_config.number_pixel)
 
-        # Update the statevector with the values from the UQ file
-        self.load_uq_sv()
-
-    def load_uq_sv(self):
-        'Load initial guess for state vector from UQ file'
-
-        uq_obj = HdfFile(self.lua_config.spectrum_file)
-        frame_idx = self.lua_config.l1b_sid_list(self.lua_config).frame_number
-        initial_sv = uq_obj.read_double_2d("/StateVector/sampled_state_vectors")[:, frame_idx]
+    def update_initial_guess(self, new_sv):
+        'Modify the inital guess to values different than those loaded by Lua'
 
         ig_orig = self.lua_config.initial_guess
         ig_v = fp.InitialGuessValue()
-        ig_v.initial_guess = initial_sv
+        ig_v.initial_guess = new_sv
         ig_v.apriori = ig_orig.apriori
         ig_v.apriori_covariance = ig_orig.apriori_covariance
         ig_new = fp.CompositeInitialGuess()
         ig_new.add_builder(ig_v)
         self.ls.globals["initial_guess"] = ig_new
-        self.lua_config.state_vector.update_state(initial_sv)
+        self.lua_config.state_vector.update_state(new_sv)
 
     def calculate_uq_radiance(self):
         'Compute radiances for use by UQ L1B. Noise is added based on the L1B noise model'
+
+        # Load the true state vector from UQ file for the Measured RT calculation
+        orig_sv = self.lua_config.state_vector.state.copy()
+        with closing(HdfFile(self.lua_config.spectrum_file)) as uq_obj:
+            frame_idx = self.lua_config.l1b_sid_list(self.lua_config).frame_number
+            true_sv = uq_obj.read_double_2d("/StateVector/sampled_state_vectors")[:, frame_idx]
+            self.update_initial_guess(true_sv)
 
         # Change SpectralWindows so that all radiances points are computed
         spec_win_range = self.lua_config.spec_win.range_array
@@ -88,6 +89,9 @@ class UqExperiment(object):
         spec_win_range.value = orig_spec_win_value
         self.lua_config.spec_win.range_array = spec_win_range
         self.lua_config.spec_win.bad_sample_mask = orig_bad_samp_mask
+
+        # Reset initial guess to values loaded by Lua
+        self.update_initial_guess(orig_sv)
 
     def run(self):
         # Calculate the radiances need for the retrieval
