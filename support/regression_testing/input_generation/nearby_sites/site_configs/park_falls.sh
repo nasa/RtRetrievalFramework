9d6c7ae9342f43caca88dc8774604394d7ffe7ca
James McDuffie <James.McDuffie@jpl.nasa.gov>
2015-10-06 16:27:40 -0700

    Move regression testing inputs off into its own repo: https://github.jpl.nasa.gov/OCO/regression_test_inputs

--- support/regression_testing/input_generation/nearby_sites/site_configs/park_falls.sh
+++
@@ -1,9 +0,0 @@
-# Sourced by utils
-
-site_name="park_falls"
-latitude="45.945"
-longitude="-90.273"
-search_distance="500"
-
-# For finding files
-paths="33 34"
--- support/regression_testing/processing_scripts/compare_regression_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-basedir=`dirname $0`
-testdir=$basedir
-. $basedir/common.sh
-for i in "${run_list[@]}"; do
-    echo "****** Checking $i ********"
-    compare_regression_test $basedir/$i/output $testdir/$i/expected
-done
