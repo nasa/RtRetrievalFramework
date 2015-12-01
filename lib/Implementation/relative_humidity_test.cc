8809960ddb663fae6153f8d461ea08338f1afeb6
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2015-11-30 16:00:42 -0800

    Have skeleton of RelativeHumidity in place. Doesn't actually do anything yet.

---
+++ lib/Implementation/relative_humidity_test.cc
@@ -0,0 +1,15 @@
+#include "unit_test_support.h"
+#include "atmosphere_fixture.h"
+#include "relative_humidity.h"
+
+using namespace FullPhysics;
+using namespace blitz;
+BOOST_FIXTURE_TEST_SUITE(relative_humidity, AtmosphereFixture)
+
+BOOST_AUTO_TEST_CASE(basic)
+{
+  RelativeHumidity h(atm->absorber_ptr(), atm->temperature_ptr(),
+		     atm->pressure_ptr());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
