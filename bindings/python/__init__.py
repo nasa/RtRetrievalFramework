22a5c9907e85f3a237787a74d1114f235fd12edb
James McDuffie <James.McDuffie@jpl.nasa.gov>
2016-02-23 14:39:06 -0800

    Fixes to get SWIG Python library working under Python 3

--- bindings/python/__init__.py
+++ bindings/python/__init__.py
@@ -5,6 +5,5 @@ import os
 from full_physics_swig._swig_wrap import *
 
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
-    exec 'from ' + os.path.basename(i).split('.')[0] + ' import *'
-
-
+    mname = os.path.basename(i).split('.')[0]
+    exec('from .%s import *' % mname)
