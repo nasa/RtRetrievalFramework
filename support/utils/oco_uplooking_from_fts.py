9513824d81d8536cc381e209c10f31190e05ee82
James McDuffie <James.McDuffie@jpl.nasa.gov>
2016-03-01 10:33:35 -0800

    Add Fabiano's changes to oco_uplooking_from_fts.py

--- support/utils/oco_uplooking_from_fts.py
+++ support/utils/oco_uplooking_from_fts.py
@@ -20,7 +20,7 @@ from full_physics.l2_input import L2InputFile
 from full_physics import HdfFile, SpectralWindowRange, Level1bFts, DispersionPolynomial
 
 sys.path.append(os.path.dirname(sys.argv[0]))
-from offline_ils import convolve_data
+from offline_ils import convolve_data, DEFAULT_SOUNDING
 
 DEFAULT_FTS_CONFIG_FILE = os.path.realpath(os.path.join(os.path.dirname(sys.argv[0]), '../../input/fts/config/config.lua'))
 
@@ -41,9 +41,9 @@ def spectral_window(static_input_file=FTS_STATIC_INPUT_FILE):
     win_ranges = static_inp.read_double_with_unit_3d("Spectral_Window/microwindow")
     return SpectralWindowRange(win_ranges)
 
-def create_sounding_id(obs_id, l1b_fts):
+def create_sounding_id(obs_id, l1b_fts, sounding_pos=DEFAULT_SOUNDING):
     dt = datetime.utcfromtimestamp(l1b_fts.time(0).unix_time)
-    sounding_id = '%s%03d' % (dt.strftime('%Y%m%d%H%M%S'), obs_id)
+    sounding_id = '%s%03d%d' % (dt.strftime('%Y%m%d%H%M%S'), obs_id, (sounding_pos+1))
     return sounding_id
 
 def create_datasets(obs_ids, output_file, l1b_file):
@@ -77,8 +77,9 @@ def create_datasets(obs_ids, output_file, l1b_file):
 
     for band_name in BAND_NAMES:
         output_file.create_dataset('/SoundingMeasurements/radiance_%s' % band_name, (num_frames,1,1016), dtype=float)
+        output_file.create_dataset('/SoundingMeasurements/wavelength_%s' % band_name, (num_frames,1,1016), dtype=float)
 
-def store_l1b_data(sounding_id, snd_idx, l1b_fts, output_file, l1b_file):
+def store_l1b_data(sounding_id, snd_idx, l1b_fts, output_file, l1b_file, sounding_pos=DEFAULT_SOUNDING):
 
     for band_idx in range(len(BAND_NAMES)):
         fp_time = l1b_fts.time(band_idx)
@@ -104,14 +105,16 @@ def store_l1b_data(sounding_id, snd_idx, l1b_fts, output_file, l1b_file):
         disp_poly = DispersionPolynomial(disp_coeff.value, disp_flag, disp_coeff.units, BAND_NAMES[band_idx], number_pixel, False)
         high_res_wl = disp_poly.pixel_grid.data
 
-        conv_wl, conv_rad = convolve_data(l1b_file, high_res_wl, high_res_rad, band_idx, in_wavenumber=True)
+        conv_wl, conv_rad = convolve_data(l1b_file, high_res_wl, high_res_rad, band_idx, in_wavenumber=True, sounding_pos=sounding_pos)
 
         output_file['/SoundingMeasurements/radiance_%s' % BAND_NAMES[band_idx]][snd_idx, 0, :] = conv_rad
+        output_file['/SoundingMeasurements/wavelength_%s' % BAND_NAMES[band_idx]][snd_idx, 0, :] = conv_wl
 
     output_file['/FrameHeader/frame_time'][snd_idx] = fp_time.pgs_time
     output_file['/FrameHeader/frame_time_stamp'][snd_idx] = bytes(fp_time)
 
-def convert_fts_data(fts_run_dir, output_filename, l1b_filename=REFERENCE_L1B_FILE, config_filename=DEFAULT_FTS_CONFIG_FILE):
+def convert_fts_data(fts_run_dir, output_filename, l1b_filename=REFERENCE_L1B_FILE, config_filename=DEFAULT_FTS_CONFIG_FILE,
+                     window_filename=DEFAULT_FTS_CONFIG_FILE, sounding_pos=DEFAULT_SOUNDING):
     # Open files needed for processing
     output_file = h5py.File(output_filename, 'w')
     l1b_file = h5py.File(l1b_filename, 'r')
@@ -132,20 +135,20 @@ def convert_fts_data(fts_run_dir, output_filename, l1b_filename=REFERENCE_L1B_FI
 
     logger.debug('Run Log = %s' % runlog_file)
 
-    spec_win = spectral_window()
+    spec_win = spectral_window(window_filename)
 
     # Load the FTS L1B object for each sounding, writing that information to the output file
     for idx, (obs_id, spec_a, spec_b) in enumerate(zip(obs_ids, spec_a_files, spec_b_files)):
         spectra_names = [spec_b, spec_a, spec_a]
 
         l1b_fts = Level1bFts(runlog_file, spectra_names, spec_win.spectral_bound)
-        sounding_id = create_sounding_id(obs_id, l1b_fts)
+        sounding_id = create_sounding_id(obs_id, l1b_fts,sounding_pos)
 
         logger.debug('Obs ID = %s, Sounding ID = %s' % (obs_id, sounding_id))
         logger.debug('Spectrum A = %s' % spec_a)
         logger.debug('Spectrum B = %s' % spec_b)
 
-        store_l1b_data(sounding_id, idx, l1b_fts, output_file, l1b_file)
+        store_l1b_data(sounding_id, idx, l1b_fts, output_file, l1b_file, sounding_pos=sounding_pos)
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='Converts FTS data into ')
@@ -158,6 +161,15 @@ if __name__ == "__main__":
     parser.add_argument('--l1b_file', '-l', metavar='FILENAME', default=REFERENCE_L1B_FILE,
         help='L1B file to get ILS information from')
 
+    parser.add_argument('--window_file', '-w', metavar='FILENAME', default=FTS_STATIC_INPUT_FILE,
+        help='static input file to get window information from')
+
+    parser.add_argument('--config_file', '-c', metavar='FILENAME', default=DEFAULT_FTS_CONFIG_FILE,
+        help='static input file to get window information from')
+
+    parser.add_argument('--sounding_pos', '-s', metavar='INT', default=DEFAULT_SOUNDING, type=int, choices=range(8),
+        help='specify which footprint ils to use (zero-based index)')
+
     parser.add_argument('--verbose', '-v', default=False, action='store_true',
         help='Output verbose debugging information')
 
@@ -168,4 +180,4 @@ if __name__ == "__main__":
     else:
         log_util.init_logging(logging.INFO)
 
-    convert_fts_data(args.fts_dir, args.output_file, args.l1b_file)
+    convert_fts_data(args.fts_dir, args.output_file, args.l1b_file, args.config_file, args.window_file, sounding_pos=args.sounding_pos)
