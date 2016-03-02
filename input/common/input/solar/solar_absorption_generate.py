dcdf3d23105e9bc7bc88909a3fd5bd324f8c44be
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-03-02 15:38:36 -0800

    Update solar_absorption_generate.py.

--- input/common/input/solar/solar_absorption_generate.py
+++ input/common/input/solar/solar_absorption_generate.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+#
 # This code was used to generate the initial table for the absorption. This
 # just evaluates old SolarAbsorptionOcoFile at closely spaced points.
 # This will most likely be changed in the future, but we need to be able
@@ -6,17 +8,38 @@ from full_physics import *
 import numpy as np
 import h5py
 
+version = "March 2, 2016"
+usage = '''Usage:
+  solar_absorption_generate.py [options] <solar_line_list> <output_file>
+  solar_absorption_generate.py -h | --help
+  solar_absorption_generate.py -v | --version
+
+This program is used to generate the solar absorption. We use Geoff Toon's
+code (wrapped in the class SolarAbsorptionGfitFile) and line list to
+generate a file. The solar_line_list should be given (e..g, solar_merged.108),
+and the output file (e.g., l2_solar_model.h5).
+
+Options:
+  -h --help         
+     Print this message
+
+  -v --version      
+     Print program version
+'''
+
+args = docopt_simple(usage, version=version)
+
 output_file = "solar_model_tables.h5"
 if len(sys.argv) > 1:
     output_file = sys.argv[1]
 
-print "Writing to: %s" % output_file
-if os.path.exists(output_file):
-    hout = h5py.File(output_file, "r+")
+print("Writing to: %s" % args.output_file)
+if os.path.exists(args.output_file):
+    hout = h5py.File(args.output_file, "r+")
 else:
-    hout = h5py.File(output_file, "w")
+    hout = h5py.File(args.output_file, "w")
 
-s = SolarAbsorptionGfitFile(os.path.join(os.path.dirname(sys.argv[0]), "solar_merged.108"), 1.0)
+s = SolarAbsorptionGfitFile(args.solar_line_list, 1.0)
 
 band1 = np.arange(12700, 13300, 0.001)
 band2 = np.arange(5800, 6500, 0.001)
@@ -43,5 +66,7 @@ for ds_name, data, unit in output_data:
     ds.attrs['Units'] = unit
     if ds_name.find("wavenumber") >= 0:
         ds.attrs['Frame'] = "Solar rest frame"
+    else:
+        ds.attrs['Solar line list input'] = args.solar_line_list
 
 hout.close()
