9b2cf37cce3df185b5bf15f1525b91f640670b85
James McDuffie <James.McDuffie@jpl.nasa.gov>
2016-03-08 12:32:35 -0800

    Switch on C+11 flag for compilation. Fix C++11 issues.

--- lib/Interface/ils.cc
+++ lib/Interface/ils.cc
@@ -6,9 +6,11 @@ using namespace FullPhysics;
 REGISTER_LUA_CLASS(Ils)
 REGISTER_LUA_END()
 
-REGISTER_LUA_CLASS_NAME(std::vector<boost::shared_ptr<Ils> >,
-			VectorIls)
+// typedef to distinguish between copying value or moving value (C++11) push_back prototoypes 
+typedef void(std::vector<boost::shared_ptr<Ils> >::*pbt1)(
+        const std::vector<boost::shared_ptr<Ils> >::value_type&);
+REGISTER_LUA_CLASS_NAME(std::vector<boost::shared_ptr<Ils> >, VectorIls)
 .def(luabind::constructor<>())
-.def("push_back", &std::vector<boost::shared_ptr<Ils> >::push_back)
+.def("push_back", ((pbt1) &std::vector<boost::shared_ptr<Ils> >::push_back))
 REGISTER_LUA_END()
 #endif
--- lib/Interface/instrument_correction.cc
+++ lib/Interface/instrument_correction.cc
@@ -6,15 +6,19 @@ using namespace FullPhysics;
 REGISTER_LUA_CLASS(InstrumentCorrection)
 REGISTER_LUA_END()
 
-REGISTER_LUA_CLASS_NAME(std::vector<boost::shared_ptr<InstrumentCorrection> >,
-			VectorInstrumentCorrection)
+// typedef to distinguish between copying value or moving value (C++11) push_back prototoypes 
+typedef void(std::vector<boost::shared_ptr<InstrumentCorrection> >::*pbt1)(
+        const std::vector<boost::shared_ptr<InstrumentCorrection> >::value_type&);
+REGISTER_LUA_CLASS_NAME(std::vector<boost::shared_ptr<InstrumentCorrection> >, VectorInstrumentCorrection)
 .def(luabind::constructor<>())
-.def("push_back", &std::vector<boost::shared_ptr<InstrumentCorrection> >::push_back)
+.def("push_back", ((pbt1) &std::vector<boost::shared_ptr<InstrumentCorrection> >::push_back))
 REGISTER_LUA_END()
 
-REGISTER_LUA_CLASS_NAME(std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > >,
-			VectorVectorInstrumentCorrection)
+// typedef to distinguish between copying value or moving value (C++11) push_back prototoypes 
+typedef void(std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > >::*pbt2)(
+        const std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > >::value_type&);
+REGISTER_LUA_CLASS_NAME(std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > >, VectorVectorInstrumentCorrection)
 .def(luabind::constructor<>())
-.def("push_back", &std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > >::push_back)
+.def("push_back", ((pbt2) &std::vector<std::vector<boost::shared_ptr<InstrumentCorrection> > >::push_back))
 REGISTER_LUA_END()
 #endif
