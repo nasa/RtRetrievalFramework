450178341c4bee0b288162399607ae58d7e90bb9
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2016-03-10 10:49:52 -0800

    Oops, checked in a testing version of lua config for the oco sounding 1 test. Reverse this.

--- test/oco2_sounding_1/config.lua
+++ test/oco2_sounding_1/config.lua
@@ -9,7 +9,4 @@ config.fm.instrument.instrument_correction.ic_nadir = {}
 config.fm.instrument.instrument_correction.ic_glint = {}
 config.fm.instrument.instrument_correction.ic_target = {}
 
-config.static_merra_aerosol_file = config_common_dir .. "/../input/l2_aerosol_combined_RH.h5"
-config.fm.atmosphere.aerosol.relative_humidity_aerosol = true
-
 config:do_config()
