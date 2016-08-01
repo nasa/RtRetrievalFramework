ca2630f3b3674d5d7c6087a4f936dec6087ba9b2
James McDuffie <James.McDuffie@jpl.nasa.gov>
2016-08-01 09:27:43 -0700

    Don't automatically import fp_perturbation into the full_physics package since it has SWIG interface specific stuff in it. This can break tools when they don't use or need the SWIG interface

--- lib/Python/__init__.py
+++ lib/Python/__init__.py
@@ -14,7 +14,12 @@ from . import safe_matplotlib_import
 # full_physics_swig stuff (with __all__, *only* the listed modules are
 # included). So we just explicitly import the modules we want here.
 
+# Don't automatically import these modules, they may use C interface
+# stuff and should not be available unless directly imported
+NO_AUTO_IMPORT = [ "fp_perturbation" ]
+
 for i in glob.glob(os.path.dirname(__file__) + "/*.py"):
     mname = os.path.basename(i).split('.')[0]
-    if(not re.match('.*_test', mname)):
+    # Don't automatically import test routines
+    if(not re.match('.*_test', mname)) and (not mname in NO_AUTO_IMPORT):
         exec('from .%s import *' % mname)
