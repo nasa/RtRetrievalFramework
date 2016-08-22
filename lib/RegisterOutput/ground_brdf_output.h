ba924725f838aa4aab67dcd55256394a5bbc2d88
James McDuffie <James.McDuffie@jpl.nasa.gov>
2016-08-22 12:45:23 -0700

    Adds effective_albedo datasets to output in BRDF mode, adds to ticket #2162

--- lib/RegisterOutput/ground_brdf_output.h
+++ lib/RegisterOutput/ground_brdf_output.h
@@ -13,13 +13,17 @@ namespace FullPhysics {
 *******************************************************************/
 class GroundBrdfOutput : public RegisterOutputBase {
 public:
-  GroundBrdfOutput(const boost::shared_ptr<GroundBrdf>& Brdf, const std::vector<std::string>& Hdf_band_names) 
-    : brdf(Brdf), hdf_band_names(Hdf_band_names) {}
+  GroundBrdfOutput(const boost::shared_ptr<GroundBrdf>& Brdf, const blitz::Array<double, 1>& Sza, const blitz::Array<double, 1>& Eff_alb_table_cos_sza, const blitz::Array<double, 1>& Eff_alb_table_intensity_scaling, const std::vector<std::string>& Hdf_band_names) 
+    : brdf(Brdf), sza(Sza), eff_alb_table_cos_sza(Eff_alb_table_cos_sza), eff_alb_table_intensity_scaling(Eff_alb_table_intensity_scaling), hdf_band_names(Hdf_band_names) {}
   virtual ~GroundBrdfOutput() {}
+  double interpolated_intensity_scaling(int spec_idx) const;
   virtual void register_output(const boost::shared_ptr<Output>& out) const;
   virtual void register_output_apriori(const boost::shared_ptr<Output>& out) const;
 private:
   boost::shared_ptr<GroundBrdf> brdf;
+  const blitz::Array<double, 1>& sza;
+  const blitz::Array<double, 1>& eff_alb_table_cos_sza;
+  const blitz::Array<double, 1>& eff_alb_table_intensity_scaling;
   std::vector<std::string> hdf_band_names;
   mutable std::string surface_type;
 };
