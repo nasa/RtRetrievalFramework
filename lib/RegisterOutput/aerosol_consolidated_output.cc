bbc9517bf830ececb0299fb0953f18c96d456168
James McDuffie <James.McDuffie@jpl.nasa.gov>
2017-04-13 17:20:09 +0000

    Changes to consolidated aerosol output: Move AerosolModels from metadata into per single sounding value in RetrievalResults, make it the size of all aerosol types. Remove retrieved aerosol types list and instead replace with a dataset that flags which of the aerosols from AllAerosolTypes are retrieved.

--- lib/RegisterOutput/aerosol_consolidated_output.cc
+++ lib/RegisterOutput/aerosol_consolidated_output.cc
@@ -92,6 +92,8 @@ public:
     }
 
     blitz::Array<std::string, 1> aerosol_model_types() const { return aer_model_types_; }
+    blitz::Array<int, 1> aerosol_type_retrieved() const { return type_retrieved_; }
+
     blitz::Array<std::string, 1> all_aerosol_names() const { 
         // Convert to blitz array for output
         blitz::Array<std::string, 1> out_aer_names(all_aer_names.size());
@@ -106,19 +108,28 @@ private:
     {
         ret_aer_names = aerosol->aerosol_name();
 
+        aer_model_types_.resize(all_aer_names.size());
+        aer_model_types_ = "";
 
-        // Make this array match the retrieved aerosol types length
-        aer_model_types_.resize(ret_aer_names.size());
+        type_retrieved_.resize(all_aer_names.size());
+        type_retrieved_ = 0;
 
         // Find the maximum number of parameters and also fill in the model types array
         max_num_param = 0;
-        for(int aer_idx = 0; aer_idx < ret_aer_names.size(); aer_idx++) {
-            auto aer_ext = boost::dynamic_pointer_cast<AerosolExtinctionImpBase>(aerosol->aerosol_extinction(aer_idx));
-            if (!aer_ext)
-                throw Exception("Could not ast aerosol extinction into AerosolExctinctionImpBase");
-            max_num_param = max(max_num_param, aer_ext->aerosol_parameter().rows());
+        for(int out_aer_idx = 0; out_aer_idx < all_aer_names.size(); out_aer_idx++) {
+            auto match_iter = std::find(ret_aer_names.begin(), ret_aer_names.end(), all_aer_names[out_aer_idx]);
 
-            aer_model_types_(aer_idx) = aer_ext->model_short_name();
+            if(match_iter != ret_aer_names.end()) {
+                int in_aer_idx = std::distance(ret_aer_names.begin(), match_iter);
+
+                auto aer_ext = boost::dynamic_pointer_cast<AerosolExtinctionImpBase>(aerosol->aerosol_extinction(in_aer_idx));
+                if (!aer_ext)
+                    throw Exception("Could not ast aerosol extinction into AerosolExctinctionImpBase");
+                max_num_param = max(max_num_param, aer_ext->aerosol_parameter().rows());
+
+                aer_model_types_(out_aer_idx) = aer_ext->model_short_name();
+                type_retrieved_(out_aer_idx) = 1;
+            }
         }
     }
 
@@ -134,7 +145,8 @@ private:
     std::vector<std::string> ret_aer_names;
     int max_num_param;
 
-    blitz::Array<std::string, 1> aer_model_types_;
+    Array<std::string, 1> aer_model_types_;
+    Array<int, 1> type_retrieved_;
   
 };
 
@@ -168,9 +180,10 @@ void AerosolConsolidatedOutput::register_output(const boost::shared_ptr<Output>&
 {
     boost::shared_ptr<AerosolOutputHelper> h(new AerosolOutputHelper(aerosol, all_aer_names));
 
-    out->register_data_source("/Metadata/RetAerosolTypes", &AerosolOptical::aerosol_name_arr, aerosol);
     out->register_data_source("/Metadata/AllAerosolTypes", &AerosolOutputHelper::all_aerosol_names, h);
-    out->register_data_source("/Metadata/AerosolModels", &AerosolOutputHelper::aerosol_model_types, h);
+
+    out->register_data_source("/RetrievalResults/aerosol_models", &AerosolOutputHelper::aerosol_model_types, h);
+    out->register_data_source("/RetrievalResults/aerosol_type_retrieved", &AerosolOutputHelper::aerosol_type_retrieved, h);
 
     out->register_data_source("/RetrievalResults/aerosol_aod", &AerosolOutputHelper::aerosol_aod_matrix, h);
     out->register_data_source("/RetrievalResults/aerosol_param", &AerosolOutputHelper::aerosol_param_matrix, h);
