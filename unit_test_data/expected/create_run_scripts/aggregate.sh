35db0b9a56544f085db17d4988a6214734cb966d
Mike <Mike.M.Smyth@jpl.nasa.gov>
2017-04-26 08:54:52 -0700

    Continue updating populate scripts to do aggregation by group

--- unit_test_data/expected/create_run_scripts/aggregate.sh
+++ unit_test_data/expected/create_run_scripts/aggregate.sh
@@ -32,16 +32,17 @@ EOF
 fi
 
 use_subdir=False
+do_aggregate=False
 
 # Aggregate all single sounding output hdf files into a single hdf file
 if [ ! -e "$l2_agg_fn" ]; then
     # Use find instead of a glob because there could be too much files that
     # could exhaust the command line length limit
     input_files_tmp=$(mktemp)
-    if [ "$use_subdir" = "True" ]; then
-       find create_run_scripts_test/output -name "l2_aggregate.h5" > $input_files_tmp
+    if [ "$do_aggregate" = "True" ]; then
+       find create_run_scripts_test/output -name "l2_aggregate_*.h5" > $input_files_tmp
     else
-       find create_run_scripts_test/output -name "*.h5" | grep -v "l2_aggregate.h5" | grep -v "l2_plus_more_aggregate.h5" > $input_files_tmp
+       find create_run_scripts_test/output -name "*.h5" | grep -v "l2_aggregate" | grep -v "l1_aggregate" > $input_files_tmp
     fi
 
     # Make a version of the sounding id file with newlines instead of spaces
@@ -61,39 +62,35 @@ fi
 
 if [ ! -e "$l2_plus_more_agg_fn" ]; then
     echo "Creating L2 plus more aggregated file"
-    # If we already have the l2_plus_more_aggregate.h5 for each of the
+    # Extract sounding ids, and reformat into a format that can be
+    # used by splice tool Take sounding ids from L2 aggregate so
+    # we know which soundings actually completed Otherwise we will
+    # have empty values for L2 datasets when we include IMAP or
+    # ABO2 data.
+
+    l2_snd_id_tmp1=$(mktemp)
+    l2_snd_id_tmp2=$(mktemp)
+    h5dump --noindex -o $l2_snd_id_tmp1 -d RetrievalHeader/sounding_id_reference $l2_agg_fn > /dev/null
+
+    # This line below, turns commas into new lines, removes empty
+    # spaces and blank lines
+    cat $l2_snd_id_tmp1 | sed 's|,|\n|g' | sed -r 's|^[ ]*||' | grep -v '^$' > $l2_snd_id_tmp2
+    rm $l2_snd_id_tmp1
+
+    # If we already have the l1_aggregate_*.h5 for each of the
     # groups l2_fp_job.sh worked on, just combine those.
-    if [ "$use_subdir" = "True" ]; then
-	input_files_tmp=$(mktemp)
-	find create_run_scripts_test/output -name "l2_plus_more_aggregate.h5" > $input_files_tmp
+    if [ "$do_aggregate" = "True" ]; then
+	inp_files_tmp=$(mktemp)
+	echo $l2_agg_fn > $inp_files_tmp
+	find create_run_scripts_test/output -name "l1_aggregate_*.h5" >> $inp_files_tmp
 
-	# Make a version of the sounding id file with newlines instead of spaces
-	# for use by splice tool
-	in_snd_id_tmp=$(mktemp)
-	cat $sounding_id_list_filename | tr ' ' '\n' > $in_snd_id_tmp
 	log_file=agg_$(basename ${l2_plus_more_agg_fn} | sed 's/\.h5$/.log/')
-	/l2_support_fake_path/utils/splice_product_files.py --single-file-type --splice-all -o $l2_plus_more_agg_fn -i $input_files_tmp -s $in_snd_id_tmp -l create_run_scripts_test/log/$log_file $* -w 16 --temp $worker_temp
+	/l2_support_fake_path/utils/splice_product_files.py --multiple-file-type --splice-all --rename-mapping --agg-names-filter -o $l2_plus_more_agg_fn -i $inp_files_tmp -s $l2_snd_id_tmp2 -l create_run_scripts_test/log/$log_file $* -w 16 --temp $worker_temp
 
-	rm $input_files_tmp
-	rm $in_snd_id_tmp
+	rm $inp_files_tmp
+	rm $l2_snd_id_tmp2
     else
 	# Otherwise, generate everything from scratch
-        
-	# Extract sounding ids, and reformat into a format that can be
-	# used by splice tool Take sounding ids from L2 aggregate so
-	# we know which soundings actually completed Otherwise we will
-	# have empty values for L2 datasets when we include IMAP or
-	# ABO2 data.
-
-	l2_snd_id_tmp1=$(mktemp)
-	l2_snd_id_tmp2=$(mktemp)
-	h5dump --noindex -o $l2_snd_id_tmp1 -d RetrievalHeader/sounding_id_reference $l2_agg_fn > /dev/null
-
-	# This line below, turns commas into new lines, removes empty
-	# spaces and blank lines
-	cat $l2_snd_id_tmp1 | sed 's|,|\n|g' | sed -r 's|^[ ]*||' | grep -v '^$' > $l2_snd_id_tmp2
-	rm $l2_snd_id_tmp1
-
 	# Combine L1B and L2 files into one file with IMAP and ABand
 	# files if they are supplied
 	inp_files_tmp=$(mktemp)
