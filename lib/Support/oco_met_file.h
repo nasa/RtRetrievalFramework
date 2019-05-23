2fa5cb651a1d4a2849e61dd9a11143a692c4506c
Mike <Mike.M.Smyth@jpl.nasa.gov>
2019-05-23 11:13:33 -0700

    Match git tag B9.2.00_sdos_testing_1 Squashed commit of the following:
    commit aada827768f106f35cc0b6d77bad1a1d1e6562c5
    Author: Mike <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Thu May 23 08:53:54 2019 -0700
    
        Update analysis stuff to work with python 3.
    
    commit 82c796700d7e8f4aef567e10d83d7b459bc77441
    Author: Mike <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Wed May 22 09:57:44 2019 -0700
    
        Update fm_errors_py expected results
    
    commit 2f250befc862a3d60723daaba304e1b87e435539
    Author: Mike <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Tue May 21 14:19:44 2019 -0700
    
        Update expected results for run_tests
    
    commit e391ed85d22799220e6d3abf2d3831c2efcb7e1c
    Author: Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Tue May 21 13:05:07 2019 -0700
    
        Change version to B9.2.00
    
    commit cf49b54fc1976d07a84da905e2bc7291842a5283
    Author: Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Tue May 21 10:53:36 2019 -0700
    
        Ticket #2593 - Update GOSAT EOFs for B9.
    
    commit 0dd0f99b66e0b296cced4708407b0f2ecdd69a57
    Author: Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Tue Feb 26 15:45:10 2019 -0800
    
        Add AerosolMetPrior.
    
    commit ba5ec9e55a116bab15223641af7d43a0eb32fa3c
    Author: Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
    Date:   Mon Feb 25 12:38:20 2019 -0800
    
        Add AerosolMetPrior. This is just a copy right now of MerraAerosol which is very similar to what we are going to want. Go ahead and create this as a placeholder, and then we will modify it.

--- lib/Support/oco_met_file.h
+++ lib/Support/oco_met_file.h
@@ -10,59 +10,62 @@ namespace FullPhysics {
 
 class OcoMetFile : public Meteorology {
 public:
-    OcoMetFile(const std::string& Fname, const boost::shared_ptr<HdfSoundingId>& Hdf_sounding_id);
-    ~OcoMetFile() {}
+  OcoMetFile(const std::string& Fname, const boost::shared_ptr<HdfSoundingId>& Hdf_sounding_id);
+  ~OcoMetFile() {}
 
-    // Define how to read various items
-    using Meteorology::pressure_levels;
-    blitz::Array<double, 1> pressure_levels() const
-        { return read_array(group_name + "/vector_pressure_levels" + name_suffix); }
-
-    using Meteorology::specific_humidity;
-    blitz::Array<double, 1> specific_humidity() const
-        { return read_array(group_name + "/specific_humidity_profile" + name_suffix); }
-
-    using Meteorology::vmr;
-    blitz::Array<double, 1> vmr(const std::string& Species) const;
-
-    blitz::Array<double, 1> ozone_mmr() const
-        { return read_array(group_name + "/ozone_profile" + name_suffix); }
-
-    virtual blitz::Array<double, 1> ozone_vmr() const;
-
-    double surface_pressure() const
-        { return read_scalar(group_name + "/surface_pressure" + name_suffix); }
-
-    double windspeed_u() const
-        { return read_scalar(group_name + "/windspeed_u" + name_suffix); }
-
-    double windspeed_v() const
-        { return read_scalar(group_name + "/windspeed_v" + name_suffix); }
-
-    using Meteorology::temperature;
-    blitz::Array<double, 1> temperature() const
-        { return read_array(group_name + "/temperature_profile" + name_suffix); }
-
-    void print(std::ostream& Os) const { Os << "OcoMetFile"; }
-
+  // Define how to read various items
+  using Meteorology::pressure_levels;
+  blitz::Array<double, 1> pressure_levels() const
+  { return read_array(group_name + "/vector_pressure_levels" + name_suffix); }
+  
+  using Meteorology::specific_humidity;
+  blitz::Array<double, 1> specific_humidity() const
+  { return read_array(group_name + "/specific_humidity_profile" + name_suffix); }
+  
+  using Meteorology::vmr;
+  blitz::Array<double, 1> vmr(const std::string& Species) const;
+  
+  blitz::Array<double, 1> ozone_mmr() const
+  { return read_array(group_name + "/ozone_profile" + name_suffix); }
+  
+  virtual blitz::Array<double, 1> ozone_vmr() const;
+  
+  double surface_pressure() const
+  { return read_scalar(group_name + "/surface_pressure" + name_suffix); }
+  
+  double windspeed_u() const
+  { return read_scalar(group_name + "/windspeed_u" + name_suffix); }
+  
+  double windspeed_v() const
+  { return read_scalar(group_name + "/windspeed_v" + name_suffix); }
+  
+  using Meteorology::temperature;
+  blitz::Array<double, 1> temperature() const
+  { return read_array(group_name + "/temperature_profile" + name_suffix); }
+  
+  void print(std::ostream& Os) const { Os << "OcoMetFile"; }
+  
+  double read_scalar(const std::string& Field) const;
+  blitz::Array<double, 1> read_array(const std::string& Field) const;
+  blitz::Array<int, 1> read_array_int(const std::string& Field) const;
+  blitz::Array<double, 2> read_array_2d(const std::string& Field) const;
+  const std::string& file_name() const { return h.file_name(); }
+  const HdfFile& hdf_file() const { return h;}
 private:
 
-    //-----------------------------------------------------------------------
-    /// OCO specific ECMWF reader routines
-    //-----------------------------------------------------------------------
-
-    double read_scalar(const std::string& Field) const;
-    blitz::Array<double, 1> read_array(const std::string& Field) const;
-
-    HdfFile h;
-    boost::shared_ptr<HdfSoundingId> hsid;
-    bool average_sounding_number;
-
-    // Name of the HDF group to read from, either "ECMWF" or "Meteorology"
-    std::string group_name;
-
-    // Suffix of the datasets, either "_ecmwf" or "_met"
-    std::string name_suffix;
+  //-----------------------------------------------------------------------
+  /// OCO specific ECMWF reader routines
+  //-----------------------------------------------------------------------
+  
+  HdfFile h;
+  boost::shared_ptr<HdfSoundingId> hsid;
+  bool average_sounding_number;
+  
+  // Name of the HDF group to read from, either "ECMWF" or "Meteorology"
+  std::string group_name;
+  
+  // Suffix of the datasets, either "_ecmwf" or "_met"
+  std::string name_suffix;
 };
 }
 #endif
