92d117183b829075caa65478ce1d71c8067f1769
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2019-09-19 11:43:06 -0700

    Ticket #2653 - Use new AerosolMetPrior and set aerosol AOD uncertainity to 0.5

--- test/oco2_brdf/config/config_brdf.lua
+++ test/oco2_brdf/config/config_brdf.lua
@@ -55,4 +55,9 @@ config.fm.instrument.instrument_correction.ic_nadir = {}
 config.fm.instrument.instrument_correction.ic_glint = {}
 config.fm.instrument.instrument_correction.ic_target = {}
 
+--- Newer OCO-2 configuration uses new fields in met file. But the old
+--- test data doesn't have these fields. So use the older merra climatology
+--- for these tests
+config.fm.atmosphere.aerosol.creator = ConfigCommon.merra_aerosol_creator
+
 config:do_config()
